7813edfcbb21adf01ac6b322fa466f58d764910b
happyhipster <yoonbin.au@gmail.com>
2021-05-07 22:52:18 +1000

    Work on the attack function next

--- battleship.py
+++ battleship.py
@@ -18,7 +18,7 @@ class Player:
 
 
 class Ship:
-    def __init__(self, ship_name, length, no_hit, x_1, y_1, x_2, y_2, list_of_coor):
+    def __init__(self, ship_name, length, no_hit, x_1, y_1, x_2, y_2, dict_of_coor):
         self.ship_name = ship_name
         self.length = length
         self.no_hit = no_hit
@@ -26,7 +26,7 @@ class Ship:
         self.y_1 = x_1
         self.x_2 = x_2
         self.y_2 = x_2
-        self.list_of_coor = list(list_of_coor)
+        self.dict_of_coor = dict(dict_of_coor)
 
 def setup():
 
@@ -48,13 +48,14 @@ def set_up_player_1():
     p1_smol_start = input("Start point of smol (x,y) ").split(',')     
     p1_smol_end = input("End point of smol (x,y) ").split(',')
 
-    p1_carrier = Ship('carrier', 6, 0, p1_carrier_start[0], p1_carrier_start[1], p1_carrier_end[0], p1_carrier_end[1], [])
-    p1_battleship = Ship('battleship', 5, 0, p1_battleship_start[0], p1_battleship_start[1], p1_battleship_end[0], p1_battleship_end[1], [])
-    p1_destroyer = Ship('destroyer', 4, 0, p1_destroyer_start[0], p1_destroyer_start[1], p1_destroyer_end[0], p1_destroyer_end[1], [])
-    p1_submarine = Ship('submarine', 3, 0, p1_submarine_start[0], p1_submarine_start[1], p1_submarine_end[0], p1_submarine_end[1], [])
-    p1_smol = Ship('smol', 2, 0, p1_smol_start[0], p1_smol_start[1], p1_smol_end[0], p1_smol_end[1], [])
+    p1_carrier = Ship('carrier', 6, 0, p1_carrier_start[0], p1_carrier_start[1], p1_carrier_end[0], p1_carrier_end[1], {})
+    p1_battleship = Ship('battleship', 5, 0, p1_battleship_start[0], p1_battleship_start[1], p1_battleship_end[0], p1_battleship_end[1], {})
+    p1_destroyer = Ship('destroyer', 4, 0, p1_destroyer_start[0], p1_destroyer_start[1], p1_destroyer_end[0], p1_destroyer_end[1], {})
+    p1_submarine = Ship('submarine', 3, 0, p1_submarine_start[0], p1_submarine_start[1], p1_submarine_end[0], p1_submarine_end[1], {})
+    p1_smol = Ship('smol', 2, 0, p1_smol_start[0], p1_smol_start[1], p1_smol_end[0], p1_smol_end[1], {})
     global p1_ships
-    p1_ships = [p1_carrier, p1_battleship, p1_destroyer, p1_submarine, p1_smol]
+    #True means alive, False means sunk.
+    p1_ships = {p1_carrier:True, p1_battleship:True, p1_destroyer:True, p1_submarine:True, p1_smol:True}
     
 
 def set_up_player_2():
@@ -70,18 +71,19 @@ def set_up_player_2():
     p2_smol_start = input("Start point of smol (x,y) ").split(',')
     p2_smol_end = input("End point of smol (x,y) ").split(',')
 
-    p2_carrier = Ship('carrier', 6, 0, p2_carrier_start[0], p2_carrier_start[1], p2_carrier_end[0], p2_carrier_end[1], [])
-    p2_battleship = Ship('battleship', 5, 0, p2_battleship_start[0], p2_battleship_start[1], p2_battleship_end[0], p2_battleship_end[1], [])
-    p2_destroyer = Ship('destroyer', 4, 0, p2_destroyer_start[0], p2_destroyer_start[1], p2_destroyer_end[0], p2_destroyer_end[1], [])
-    p2_submarine = Ship('submarine', 3, 0, p2_submarine_start[0], p2_submarine_start[1], p2_submarine_end[0], p2_submarine_end[1], [])
-    p2_smol = Ship('smol', 2, 0, p2_smol_start[0], p2_smol_start[1], p2_smol_end[0], p2_smol_end[1], [])
+    p2_carrier = Ship('carrier', 6, 0, p2_carrier_start[0], p2_carrier_start[1], p2_carrier_end[0], p2_carrier_end[1], {})
+    p2_battleship = Ship('battleship', 5, 0, p2_battleship_start[0], p2_battleship_start[1], p2_battleship_end[0], p2_battleship_end[1], {})
+    p2_destroyer = Ship('destroyer', 4, 0, p2_destroyer_start[0], p2_destroyer_start[1], p2_destroyer_end[0], p2_destroyer_end[1], {})
+    p2_submarine = Ship('submarine', 3, 0, p2_submarine_start[0], p2_submarine_start[1], p2_submarine_end[0], p2_submarine_end[1], {})
+    p2_smol = Ship('smol', 2, 0, p2_smol_start[0], p2_smol_start[1], p2_smol_end[0], p2_smol_end[1], {})
     global p2_ships
-    p2_ships = [p2_carrier, p2_battleship, p2_destroyer, p2_submarine, p2_smol]
+    #True means alive, False means sunk.
+    p2_ships = {p2_carrier:True, p2_battleship:True, p2_destroyer:True, p2_submarine:True, p2_smol:True}
 
 def setup_coordinates_p1():
     global p1_ships #attempt. This part i need to figure out how to use ship_type to call the class... y'know... cuz i dont
     for i in range(5):
-        ship_type = p1_ships[i]
+        ship_type = list(dict_of_coor)[o]
         if ship_type.x_1 == ship_type.x_2:
             ship_direction = "vertical"
         elif ship_type.y_1 == ship_type.y_1:
@@ -91,20 +93,20 @@ def setup_coordinates_p1():
             while ship_type.y_1 != ship_type.y_2:
                 y_1 += 1
                 coor = str(ship_type.x_1 + ',' + ship_type.y_1)
-                ship_type.list_of_coor.append(coor)
+                ship_type.dict_of_coor[coor] = True #if this does not work try the update dictionary method
 
         if ship_direction == "vertical":
             while ship_type.x_1 != ship_type.x_2:
                 ship_type.x_1 += 1
                 coor = str(ship_type.x_1 + ',' + ship_type.y_1)
                 print(coor) #debuggin
-                ship_type.list_of_coor.append(coor)
+                ship_type.dict_of_coor[coor] = True
 
     
 def setup_coordinates_p2(): #attempt
     global p2_ships
     for o in range(5):
-        ship_type = p2_ships[o]
+        ship_type = list(dict_of_coor)[o]
         if ship_type.x_1 == ship_type.x_2:
             ship_direction = "vertical"
         elif ship_type.y_1 == ship_type.y_1:
@@ -114,46 +116,46 @@ def setup_coordinates_p2(): #attempt
             while ship_type.y_1 != ship_type.y_2:
                 ship_type.y_1 += 1
                 coor = str(ship_type.x_1 + ',' + ship_type.y_1)
-                ship_type.list_of_coor.append(coor)
-                print(ship_type.list_of_coor)
+                ship_type.dict_of_coor.append(coor)
+                ship_type.dict_of_coor[coor] = True
 
         elif ship_direction == "vertical":
             while ship_type.x_1 != ship_type.x_2:
                 ship_type.x_1 += 1
                 coor = str(ship_type.x_1 + ',' + ship_type.y_1)
-                ship_type.list_of_coor.append(coor)
+                ship_type.dict_of_coor[coor] = True
 
 def p1_checking_phase(): #try using pop if remove does not work and also try using a dictionary to tell the status of ships (sunk/float)
-    if len(p1_carrier.list_of_coor) == 0:
+    if len(p1_carrier.dict_of_coor) == 0:
         print("Carrier sunk ")
         p1_ships.remove(p1_carrier)
-    elif len(p1_battleship.list_of_coor) == 0:
+    elif len(p1_battleship.dict_of_coor) == 0:
         print("Battleship sunk ")
         p1_ships.remove(p1_battleship)
-    elif len(p1_destroyer.list_of_coor) == 0:
+    elif len(p1_destroyer.dict_of_coor) == 0:
         print("Destroyer sunk ")
         p1_ships.remove(p1_destroyer)
-    elif len(p1_submarine.list_of_coor) == 0:
+    elif len(p1_submarine.dict_of_coor) == 0:
         print("Submarine sunk ")
         p1_ships.remove(p1_submarine)
-    elif len(p1_smol.list_of_coor) == 0:
+    elif len(p1_smol.dict_of_coor) == 0:
         print("Smol sunk ")
         p1_ships.remove(p1_smol)
     
 def p2_checking_phase():
-    if len(p2_carrier.list_of_coor) == 0:
+    if len(p2_carrier.dict_of_coor) == 0:
         print("Carrier sunk ")
         p2_ships.remove(p2_carrier)
-    elif len(p2_battleship.list_of_coor) == 0:
+    elif len(p2_battleship.dict_of_coor) == 0:
         print("Battleship sunk ")
         p2_ships.remove(p2_battleship)
-    elif len(p2_destroyer.list_of_coor) == 0:
+    elif len(p2_destroyer.dict_of_coor) == 0:
         print("Destroyer sunk ")
         p2_ships.remove(p2_destroyer)
-    elif len(p2_submarine.list_of_coor) == 0:
+    elif len(p2_submarine.dict_of_coor) == 0:
         print("Submarine sunk ")
         p2_ships.remove(p2_submarine)
-    elif len(p2_smol.list_of_coor) == 0:
+    elif len(p2_smol.dict_of_coor) == 0:
         print("Smol sunk ")
         p2_ships.remove(p2_smol)
 
@@ -171,27 +173,29 @@ def attack(p):
     global p1_smol
     attack_pos = input("Attack! (x,y) ")
     
-    if p == "1":
-        #checking if the coordinates are in p2's battleship list
-        if attack_pos in p2_carrier.list_of_coor:
-            print("Hit! ")
-            p2_carrier.list_of_coor.remove(attack_pos)
-            
-        elif attack_pos in p2_battleship.list_of_coor:
-            print("Hit! ")
-            p2_battleship.list_of_coor.remove(attack_pos)
 
-        elif attack_pos in p2_destroyer.list_of_coor:
+    #WORK ON THIS PART NEXT
+    if p == "1":
+        #checking if the coordinates are in p2's battleship list while using dictionary
+        if attack_pos in p2_carrier.dict_of_coor:
             print("Hit! ")
-            p2_destroyer.list_of_coor.remove(attack_pos)
+            p2_carrier.dict_of_coor.remove(attack_pos)
+            
+        elif attack_pos in p2_battleship.dict_of_coor:
+            print("Hit! ")
+            p2_battleship.dict_of_coor.remove(attack_pos)
+
+        elif attack_pos in p2_destroyer.dict_of_coor:
+            print("Hit! ")
+            p2_destroyer.dict_of_coor.remove(attack_pos)
         
-        elif attack_pos in p2_submarine.list_of_coor:
+        elif attack_pos in p2_submarine.dict_of_coor:
             print("Hit! ")
-            p2_submarine.list_of_coor.remove(attack_pos)
+            p2_submarine.dict_of_coor.remove(attack_pos)
         
-        elif attack_pos in p2_smol.list_of_coor:
+        elif attack_pos in p2_smol.dict_of_coor:
             print("Hit! ")
-            p2_smol.list_of_coor.remove(attack_pos)
+            p2_smol.dict_of_coor.remove(attack_pos)
 
         else:
             print("Miss ")
@@ -199,25 +203,25 @@ def attack(p):
     
     if p == "2":
         #checking if the coordinates are in p2's battleship list
-        if attack_pos in p1_carrier.list_of_coor:
+        if attack_pos in p1_carrier.dict_of_coor:
             print("Hit! ")
-            p1_carrier.list_of_coor.remove(attack_pos)
+            p1_carrier.dict_of_coor.remove(attack_pos)
             
-        elif attack_pos in p1_battleship.list_of_coor:
+        elif attack_pos in p1_battleship.dict_of_coor:
             print("Hit! ")
-            p1_battleship.list_of_coor.remove(attack_pos)
+            p1_battleship.dict_of_coor.remove(attack_pos)
 
-        elif attack_pos in p1_destroyer.list_of_coor:
+        elif attack_pos in p1_destroyer.dict_of_coor:
             print("Hit! ")
-            p1_destroyer.list_of_coor.remove(attack_pos)
+            p1_destroyer.dict_of_coor.remove(attack_pos)
         
-        elif attack_pos in p1_submarine.list_of_coor:
+        elif attack_pos in p1_submarine.dict_of_coor:
             print("Hit! ")
-            p1_submarine.list_of_coor.remove(attack_pos)
+            p1_submarine.dict_of_coor.remove(attack_pos)
         
-        elif attack_pos in p1_smol.list_of_coor:
+        elif attack_pos in p1_smol.dict_of_coor:
             print("Hit! ")
-            p1_smol.list_of_coor.remove(attack_pos)
+            p1_smol.dict_of_coor.remove(attack_pos)
 
         else:
             print("Miss ")    
@@ -232,6 +236,7 @@ def main():
     set_up_player_2()
     setup_coordinates_p1()
     setup_coordinates_p2()
+    #while True
     attack("1")
     p2_checking_phase()
     attack("2")
